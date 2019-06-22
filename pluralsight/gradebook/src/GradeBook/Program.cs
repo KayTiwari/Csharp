2c74ffb34bfd4dd9559159aaa5719f7b0c50ae0e
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-22 15:38:17 -0600

    build .netfile

--- pluralsight/gradebook/src/GradeBook/Program.cs
+++ pluralsight/gradebook/src/GradeBook/Program.cs
@@ -6,7 +6,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fuck you!");
+            Console.WriteLine("fuck you");
         }
     }
 }
