1c33afcf09e2b7609939a61d9142560d53286f2d
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-21 18:31:48 -0600

    hello world

---
+++ mosh/fuckyou.cs
@@ -0,0 +1,23 @@
+using System;
+// namespace for basic utilities
+
+using System.Collections.Generic;
+// used to work with lists and collections
+
+using System.Linq;
+// used to work with data 
+
+using System.Text;
+// used to work with text
+
+using System.Threading.Tasks;
+// used to build multi threaded applications
+
+namespace fuckyou{
+    class Program{
+        static void Main(string[] args){
+            //wannabe java shit
+            Console.WriteLine("Fuck you");
+        }
+    }
+}
