e8d6988b257c73dff89489866b3bb3f3225f08cd
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-22 17:52:25 -0600

    calculating average value of list

--- pluralsight/gradebook/src/GradeBook/Program.cs
+++ pluralsight/gradebook/src/GradeBook/Program.cs
@@ -7,8 +7,8 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
-            var grades = new List<double>();
+            // var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
 
             // List<double> grades = new List<double>();
             //requires type argument ex: integers, strings, var, double floating points
@@ -20,6 +20,8 @@ namespace GradeBook
             foreach(var number in grades){
                 result += number;
             }
+            var final = result/grades.Count;
+            Console.WriteLine(final);
         }
         //Method name is "Main"
     }
