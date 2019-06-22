9947669d9f1da238c69675f23edc92f0e8b67d68
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-22 15:59:44 -0600

    c# debugging

--- pluralsight/gradebook/src/GradeBook/Program.cs
+++ pluralsight/gradebook/src/GradeBook/Program.cs
@@ -6,8 +6,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(args.Length);
             Console.WriteLine($"Hello {args[0]}");
         }
         //Method name is "Main"
     }
 }
+
+//Launch.json allowsyou to pass default arguments into debugger
