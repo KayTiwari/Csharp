8d29377ced51b314ea095030aa739fa7d228bb7a
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-22 23:18:27 -0600

    creating tests with xunit

---
+++ pluralsight/gradebook/test/gradebook.tests/BookTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace Gradebook.Tests
+{
+    public class BookTest
+    {
+        [Fact]
+        public void Test1(){
+
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            var result = book.ShowStatistics();
+            
+
+            Assert.Equal(85.6, result.Average);
+        }
+    }
+}
